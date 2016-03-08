4f0d81e9df33bf56649e3c5f6cf7730173a06f06
najeira <najeira@gmail.com>
2016-03-08 16:42:53 +0900

    remove Application.Run

--- app.go
+++ app.go
@@ -1,9 +1,5 @@
 package hikaru
 
-import (
-	"net/http"
-)
-
 type Application struct {
 	*Module
 }
@@ -11,14 +7,5 @@ type Application struct {
 func New() *Application {
 	return &Application{
 		Module: NewModule("/"),
-	}
-}
-
-func (app *Application) Run(addr string) {
-	// ListenAndServe will block
-	err := http.ListenAndServe(addr, app)
-
-	if err != nil {
-		panic(err)
 	}
 }
